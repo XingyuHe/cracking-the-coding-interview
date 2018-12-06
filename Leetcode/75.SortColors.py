29f0349738da859afcdd4ba32c8dd1809824a2c4
Xingyu He <xhe@mac-21009.hamilton.edu>
2018-12-05 23:38:55 -0500

    More problems

--- Leetcode/75.SortColors.py
+++ Leetcode/75.SortColors.py
@@ -3,10 +3,28 @@ def sortColors(nums):
     :type nums: List[int]
     :rtype: void Do not return anything, modify nums in-place instead.
     """
-    freq = {}
-    for n in nums:
-        freq[n] = 1 + freq.get(freq[n], 0)
+    i = j = 0
+    k = len(nums)- 1
 
-    return freq[0] * [0] + freq[1] * [1] + freq[2] * [2]
+    while j <= k:
+
+        if nums[j] == 0:
+
+            nums[j] = nums[i]
+            nums[i] = 0
+            i += 1
+            j += 1
+
+        elif nums[j] == 2:
+
+            nums[j] = nums[k]
+            nums[k] = 2
+            k -= 1
+
+        else:
+
+            j += 1
+
+    return nums
 
 print sortColors([0, 1,2,1,0])
